520e87b4ac18db76fa4334b7254f145df3e458e5
Michal Bajcar <michalb@salsitasoft.com>
2025-11-12 12:47:39 +0100

    Fix GraphQL syntax error in node() query

--- app/routes/api.search-materials.tsx
+++ app/routes/api.search-materials.tsx
@@ -198,7 +198,6 @@ export async function loader({ request }: LoaderFunctionArgs) {
                     }
                   }
                 }
-              }
             }
           }
         }
