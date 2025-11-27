ac05701ba02cef98264c04bd3fa4a5e9750b84cd
DavidB <david.br5@seznam.cz>
2025-11-27 09:56:23 +0100

    Search optimization (in progress)

---
+++ app/graphql/queries/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Export všech GraphQL queries pro snadný import
+ */
+
+export { GET_NODE_BY_ID } from "./getNodeById";
+export { GET_COLLECTION_PRODUCTS } from "./getCollectionProducts";
+export { SEARCH_PRODUCTS } from "./searchProducts";
